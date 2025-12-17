4f880f3adb3b946ee948de648c3b91ce124f8c6f
getthecodeforbiju <anandhubiju793@gmail.com>
2025-12-17 17:03:44 +0530

    update router to include review

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, users, products, admin, categories, cart, orders
+from app.api.v1.endpoints import auth, users, products, admin, categories, cart, orders, reviews
 
 api_router = APIRouter()
 
@@ -10,3 +10,4 @@ api_router.include_router(products.router)
 api_router.include_router(categories.router)
 api_router.include_router(cart.router)
 api_router.include_router(orders.router)
+api_router.include_router(reviews.router)
